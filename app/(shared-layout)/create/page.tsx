e2558e461bdaec515701669e2d2b9edb563d4c17
mehdi maroufi <mehdimaroufi.it@gmail.com>
2025-12-11 16:58:11 +0330

    Adds blog post creation form with validation
    Introduces a structured form for creating new blog posts,
    including client-side validation for title and content fields
    to improve data consistency and user feedback.

--- app/(shared-layout)/create/page.tsx
+++ app/(shared-layout)/create/page.tsx
@@ -1,10 +1,133 @@
+"use client";
+import { createBlogSchema } from "@/app/schemas/blog";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { Separator } from "@/components/ui/separator";
+import { Spinner } from "@/components/ui/spinner";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PenIcon } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import z from "zod";
+
 export default function CreatePage() {
+  const form = useForm<z.infer<typeof createBlogSchema>>({
+    resolver: zodResolver(createBlogSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+    },
+  });
+
+  async function onSubmit() {}
+
   return (
     <section className="container pt-24 ">
       <div className="text-right">
         <h1 className="text-right">ایجاد پست</h1>
         <p className="text-muted-foreground">یک پست جدید ایجاد کنید...</p>
       </div>
+      <Card className="max-w-2xl w-full mx-auto">
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">فرم ایجاد یک پست جدید</CardTitle>
+          <CardDescription>فیلد های زیر را با دقت کامل کنید</CardDescription>
+          <Separator className="my-3" />
+          <CardContent>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FieldGroup className="gap-y-5">
+                <Controller
+                  name="title"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field>
+                      <FieldLabel>عنوان</FieldLabel>
+                      <InputGroup className="h-11">
+                        <InputGroupInput
+                          placeholder="یک عنوان برای پست بنویسید ..."
+                          className="h-11 text-base pr-1"
+                          {...field}
+                          aria-invalid={fieldState.invalid}
+                          type="text"
+                          tabIndex={1}
+                        />
+                        <InputGroupAddon className="pr-2 pl-2">
+                          <PenIcon />
+                        </InputGroupAddon>
+                      </InputGroup>
+                      {fieldState.invalid && (
+                        <FieldError
+                          errors={[fieldState.error]}
+                          className="p-0 m-0 text-xs"
+                        />
+                      )}
+                    </Field>
+                  )}
+                />
+                <Controller
+                  name="content"
+                  control={form.control}
+                  render={({ field, fieldState }) => (
+                    <Field>
+                      <FieldLabel> توضیحات</FieldLabel>
+                      <Textarea
+                        className="min-h-26"
+                        placeholder="متن و توضیحات کامل پست خود را بنویسید ..."
+                        {...field}
+                        aria-invalid={fieldState.invalid}
+                      />
+                      {fieldState.invalid && (
+                        <FieldError
+                          errors={[fieldState.error]}
+                          className="p-0 m-0 text-xs"
+                        />
+                      )}
+                    </Field>
+                  )}
+                />
+
+                {form.formState.isLoading || form.formState.isSubmitting ? (
+                  <>
+                    <Button
+                      size={"lg"}
+                      tabIndex={4}
+                      type="button"
+                      disabled={
+                        form.formState.isLoading || form.formState.isSubmitting
+                      }
+                    >
+                      <Spinner />
+                      لطفا کمی صبر کنید
+                    </Button>
+                  </>
+                ) : (
+                  <>
+                    <Button size={"lg"} tabIndex={4}>
+                      ایجاد پست
+                    </Button>
+                  </>
+                )}
+              </FieldGroup>
+            </form>
+          </CardContent>
+        </CardHeader>
+      </Card>
     </section>
   );
 }
