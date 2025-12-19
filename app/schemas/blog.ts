d065c8aa8c25854dcb8cf453e116034be20fcb01
mehdi maroufi <mehdimaroufi.it@gmail.com>
2025-12-19 10:53:38 +0330

    feat: enhance image validation in createBlogSchema with size and type checks

--- app/schemas/blog.ts
+++ app/schemas/blog.ts
@@ -1,5 +1,6 @@
 import z from "zod";
-
+const MAX_FILE_SIZE = 900 * 1024;
+const ALLOWED_TYPES = ["image/png", "image/jpeg", "image/webp", "image/jpg"];
 export const createBlogSchema = z.object({
   title: z
     .string("عنوان بین 10 تا 200 کارکتر مجاز است")
@@ -8,5 +9,19 @@ export const createBlogSchema = z.object({
   content: z
     .string("متن پست حداقل 50 کارکتر")
     .min(50, "متن پست حداقل 50 کارکتر"),
-  image: z.instanceof(File),
+  image: z.instanceof(File).superRefine((file, ctx) => {
+    if (file.size > MAX_FILE_SIZE) {
+      ctx.addIssue({
+        code: "custom", // این رو اضافه کردیم
+        message: "حداکثر حجم تصویر ۹۰۰ کیلوبایت است",
+      });
+    }
+
+    if (!ALLOWED_TYPES.includes(file.type)) {
+      ctx.addIssue({
+        code: "custom", // این هم اضافه شد
+        message: "فرمت تصویر مجاز نیست",
+      });
+    }
+  }),
 });
